1200944b733df0e22302860b503e6a267b61ca88
yago_t <50213633+yago1404@users.noreply.github.com>
2023-07-17 11:35:05 -0300

    add postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeamsModule } from './teams/teams.module';
 import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [TeamsModule, UsersModule],
+  imports: [TeamsModule, UsersModule, TypeOrmModule.forRoot({
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "postgres",
+    "password": "yago1404",
+    "database": "baskapp",
+    "entities": ["dist/**/*.entity{.ts,.js}"],
+    "synchronize": true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
