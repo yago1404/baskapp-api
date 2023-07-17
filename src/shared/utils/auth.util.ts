0e963eeb1a77f0260ab71cbda604193ff396c580
yago_t <50213633+yago1404@users.noreply.github.com>
2023-07-17 08:09:55 -0300

    Create Auth util

---
+++ src/shared/utils/auth.util.ts
@@ -0,0 +1,5 @@
+export abstract class AuthUtil {
+    static generateJWT(): string {
+        return 'ASDASDADSADADASDSADADADADSAASADAASDADADSADSASDADADADADADASDASDA';
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import {Body, Controller, Post, Res} from '@nestjs/common';
 import {CustomResponse} from "../shared/domain/models/custom.response";
 import {UsersService} from "./users.service";
+import {AuthUtil} from "../shared/utils/auth.util";
 
 @Controller('users')
 export class UsersController {
@@ -10,7 +11,13 @@ export class UsersController {
     async auth(@Res() res, @Body() body): Promise<any> {
         const { email, password } = body;
         let haveUser: boolean = await this.service.authenticate(email, password);
-        if (haveUser) return res.status(200).json(new CustomResponse(200, 'Success', { token: 'ASDADJDAKJNSKAD', refreshToken: 'asdasdasd'}));
+        if (haveUser) {
+            const token: string = AuthUtil.generateJWT();
+            return res.status(200).json(new CustomResponse(200, 'Success', {
+                token: token,
+                refreshToken: 'asdasdasd'
+            }));
+        }
         return res.status(404).json(new CustomResponse(404, 'Email ou senha inválidos'));
     }
 
