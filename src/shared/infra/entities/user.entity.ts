92ac6a4de2fad7229e9b609fe63b89ec1023448f
yago_t <50213633+yago1404@users.noreply.github.com>
2023-07-17 12:50:33 -0300

    Create user.entity.ts

---
+++ src/shared/infra/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('User')
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    height: number;
+
+    @Column()
+    weight: number;
+
+    @Column()
+    wingspan: number;
+
+    @Column()
+    birthday: Date;
+}
